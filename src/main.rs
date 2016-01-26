2db0d505d1c0aa1084d3f991ba754b1b0418af9f
Martin Tomasi <martin.tomasi@gmail.com>
2016-01-26 19:25:15 +0100

    Added cfg gates to use the right Window implementation based on platform

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 extern crate winapi;
 extern crate kernel32;
 extern crate user32;
+extern crate gdi32;
 
 // use options::Options;
-use window::open_window;
+use window::Window;
 
 mod options;
 mod window;
 
 fn main() {
-    let window = open_window::<window::win32::WinApiWindow>(800, 600);
-    loop {}
+    let mut window = Window::open(800, 600);
+    let mut is_running = true;
+    while is_running {
+        
+    }
 }
--- src/window.rs
+++ src/window.rs
@@ -1,22 +1,27 @@
-pub trait Window {
-    fn open(x_size: i32, y_size: i32) -> Self;
-    // TODO more trait methods, like blitting an image to the buffer and so on
-}
+// Export the right window implementation based on which OS we're running.
+#[cfg(target_os="windows")] pub use self::win32::Window;
+#[cfg(any(target_os="unix", target_os="linux"))] pub use self::nix::Window;
 
-pub fn open_window<W>(x_size: i32, y_size: i32) -> W where W: Window {
-    Window::open(x_size, y_size)
-}
-
-// TODO Linux implementation
-// TODO open_window function that chooses the right implementation 
-// based on operating system
-
-/// Windows stuff
-#[cfg(windows)]
-pub mod win32 {
-    const WIN_CLASS_NAME: &'static str = "rsquake-window";
+#[cfg(any(target_os="unix", target_os="linux"))] 
+mod nix {
+    // TODO implement X11 window or something of the sort
+    pub struct Window;
     
-    use super::Window;
+    impl Window {
+        pub fn open(x_size: i32, y_size: i32) -> Window {
+            println!("Unix says hi!");
+            Window
+        }
+        
+        pub fn clear(&mut self) {
+            println!("Clearing window!");
+        }
+    }
+}
+
+#[cfg(target_os="windows")]
+mod win32 {
+    const WIN_CLASS_NAME: &'static str = "rsquake-window";
     use std::ffi::OsStr;
     use std::os::windows::ffi::OsStrExt;
     
@@ -24,9 +29,11 @@ pub mod win32 {
     use winapi::*;
     use kernel32::*;
     use user32::*;
+    use gdi32::*;
     
+    /// This function converts Rust strings to UTF-16 strings understood by Windows.
     fn to_wchar(string: &str) -> Vec<u16> {
-       OsStr::new(string).encode_wide().collect()
+        OsStr::new(string).encode_wide().collect()
     }
     
     unsafe fn get_instance() -> HINSTANCE {
@@ -34,16 +41,17 @@ pub mod win32 {
         if instance.is_null() {
             panic!("GetModuleHandleW error: {}", GetLastError());
         }
-
+    
         instance
     }
-
+    
+    // This function does all the nasty Win32 API business to open a window.
     unsafe fn create_window(wnd_proc: WNDPROC, width: i32, height: i32) -> HWND {
         let instance = get_instance();
         let cursor = LoadCursorW(ptr::null_mut(), IDC_ARROW);
         let style = WS_OVERLAPPEDWINDOW | WS_VISIBLE;
         let class_name = to_wchar(WIN_CLASS_NAME).as_ptr();
-
+    
         let wc = WNDCLASSW {
             style: 0,
             lpfnWndProc: wnd_proc,
@@ -75,24 +83,38 @@ pub mod win32 {
             instance,
             ptr::null_mut()
         );
-        // TODO put stuff in window 
         ShowWindow(main_window, SW_SHOWDEFAULT);
         main_window
     }
     
-    pub struct WinApiWindow {
-        window: HWND
+    #[derive(Debug)]
+    pub struct Window {
+        window: HWND,
+        x_size: i32,
+        y_size: i32
     }
-
+    
     unsafe extern "system" fn wnd_callback(hwnd: HWND, uint: UINT, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
         DefWindowProcW(hwnd, uint, wparam, lparam)
     }
     
-    impl Window for WinApiWindow {
-        fn open(x_size: i32, y_size: i32) -> WinApiWindow {
+    impl Window {
+        /// Creates the window object and opens the window with the specified size.
+        pub fn open(x_size: i32, y_size: i32) -> Window {
             let window = unsafe { create_window(Some(wnd_callback), x_size, y_size) };
-            WinApiWindow {
-                window: window
+            Window {
+                window: window,
+                x_size: x_size,
+                y_size: y_size
+            }
+        }
+        
+        /// Clears the window, painting it black. (No colors anymore, I want them to turn black)
+        pub fn clear(&mut self) {
+            unsafe {
+                let dc = GetDC(self.window);
+                PatBlt(dc, 0, 0, self.x_size, self.y_size, BLACKNESS);
+                ReleaseDC(self.window, dc);
             }
         }
     }
