6e4bfb9f7b88b4f005dbe7b9d208a5160306ad00
Martin Tomasi <martin.tomasi@gmail.com>
2016-01-26 22:02:49 +0100

    added Timer struct with implementation for Windows

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,25 @@ extern crate kernel32;
 extern crate user32;
 extern crate gdi32;
 
-// use options::Options;
 use window::Window;
+use timer::Timer;
+use std::time::Duration;
+use std::thread;
 
 mod options;
 mod window;
+mod timer;
 
 fn main() {
-    let mut window = Window::open(800, 600);
+     let mut window = Window::open(800, 600);
     let mut is_running = true;
+    let mut timer = Timer::new();
+    let one_sec = Duration::from_millis(1000);
+    
     while is_running {
-        
+        thread::sleep(one_sec);
+        timer.tock();
+        println!("Total time: {} seconds", timer.elapsed_seconds());
+        timer.tick();
     }
 }
---
+++ src/timer.rs
@@ -0,0 +1,50 @@
+#[cfg(target_os="windows")] pub use self::win32::Timer;
+
+#[cfg(target_os="windows")]
+mod win32 {
+    use winapi::*;
+    use kernel32::*;
+    
+    pub struct Timer {
+        tick: i64,
+        tock: i64,
+        resolution: i64,
+        seconds_per_tick: f64
+    }
+    
+    impl Timer {
+        pub fn new() -> Timer {
+            let mut resolution = 0;
+            let mut tick = 0;
+            let mut tock = 0;
+            unsafe {
+                QueryPerformanceFrequency(&mut resolution);
+                QueryPerformanceCounter(&mut tick);
+                QueryPerformanceCounter(&mut tock);
+            }
+            
+            Timer {
+                resolution: resolution,
+                seconds_per_tick: 1.0 / resolution as f64,
+                tick: tick,
+                tock: 0
+            }
+        }
+        
+        pub fn tick(&mut self) {
+            unsafe { QueryPerformanceCounter(&mut self.tick); }
+        }
+        
+        pub fn tock(&mut self) {
+            unsafe { QueryPerformanceCounter(&mut self.tock); }
+        }
+        
+        pub fn elapsed_seconds(&self) -> f64 {
+            (self.tock - self.tick) as f64 * self.seconds_per_tick
+        }
+        
+        pub fn elapsed_ticks(&self) -> i64 {
+            self.tock - self.tick
+        }
+    }
+}
